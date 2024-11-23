206bd7778e2076ed81548b77c791e07548f04b70
Athira-vk <vkathiravk@gmail.com>
2024-11-24 01:04:55 +0530

    final Project

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,24 +1,35 @@
+import { useEffect, useRef } from "react";
+import useGetMessages from "../../hooks/useGetMessages";
 import Message from "./Message";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
+import useListenMessages from "../../hooks/useListenMessages";
 
 const Messages = () => {
+  const { messages, loading } = useGetMessages();
+  useListenMessages();
+  const lastMessageRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+  }, [messages]);
+
   return (
     <div className="px-4 flex-1  overflow-auto ">
-      <div className="custom-scrollbar">
-        <div className="custom-scrollbar-thumb"></div>
-      </div>
-      <div className="content">
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-      </div>
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <div ref={lastMessageRef} key={message._id}>
+            <Message message={message} />
+          </div>
+        ))}
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+      {!loading && messages.length === 0 && (
+        <p className="text-center text-white">
+          Send a message to start the conversation
+        </p>
+      )}
     </div>
   );
 };
